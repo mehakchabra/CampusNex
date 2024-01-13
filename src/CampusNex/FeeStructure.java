d3669c7dc02aaee0ac1f8f1d7cd8cb39cbfdfad5
Mchabra448 <mehak.chabra2020@vitbhopal.ac.in>
2024-01-13 19:30:45 +0530

    Fee Structure

---
+++ src/CampusNex/FeeStructure.java
@@ -0,0 +1,52 @@
+package CampusNex;
+
+import net.proteanit.sql.DbUtils;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+
+public class FeeStructure extends JFrame implements ActionListener {
+
+    FeeStructure(){
+        getContentPane().setBackground(Color.WHITE);
+
+        JLabel heading = new JLabel("Fee Structure");
+        heading.setBounds(400,10,400,30);
+        heading.setFont(new Font("Tahoma", Font.BOLD, 30));
+        add(heading);
+
+        JTable table = new JTable();
+
+        try {
+            Connectivity c = new Connectivity();
+            ResultSet resultSet = c.statement.executeQuery("select * from fee");
+            table.setModel(DbUtils.resultSetToTableModel(resultSet));
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        JScrollPane js = new JScrollPane(table);
+        js.setBounds(0,60,1000,700);
+        add(js);
+
+
+
+        setSize(1000,650);
+        setLocation(150,10);
+        setLayout(null);
+        setVisible(true);
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public static void main(String[] args) {
+        new FeeStructure();
+    }
+}
